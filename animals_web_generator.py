55c8856ec207ddce1ba43b27c2dcbfc0cdbfdabe
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-07-31 12:53:59 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,41 +1,10 @@
 from webbrowser import open as open_browser
-from functools import reduce
-from config import JSON_IMPORT_FILE, HTML_TEMPLATE, HTML_OUTPUT_FILE, DEFAULT_OUTPUT_KEYS
-from utils import load_data, save_data
-
-
-def get_animal_data(
-        animal: dict,
-        output_keys: tuple[str, ...]
-) -> str:
-    """
-    Creates a string of animal data, including all the selected data.
-    :param animal: The animal data dictionary.
-    :param output_keys: The selected data, defaults to name, type, diet, locations.
-    :return: The requested values a single string.
-    """
-    return "".join(
-        # Output in "key: value" manner
-        f"{key}: {value if type(value) is str else ", ".join(value)}\n"
-        for key, value in filter(
-            # Check if key has value
-            lambda zipped: zipped[1],
-
-            # Zip the correct key with its value
-            zip(
-                map(lambda key_string: key_string.split(":")[-1].capitalize(), output_keys),
-                map(lambda key_string: reduce(
-                    lambda dictionary, key: dictionary.get(key),
-                    key_string.split(":"),
-                    animal
-                ), output_keys)
-            )
-        )
-    )
+from config import JSON_IMPORT_FILE, HTML_TEMPLATE, HTML_OUTPUT_FILE
+from utils import load_data, save_data, get_animal_data
 
 
 def get_animals_template_data(animals: list[dict]) -> str:
-    return "\n".join(map(lambda animal: get_animal_data(animal, DEFAULT_OUTPUT_KEYS), animals))
+    return "\n".join(map(lambda animal: get_animal_data(animal), animals))
 
 
 def main():
---
+++ utils/animal.py
@@ -0,0 +1,32 @@
+from functools import reduce
+from ..config import DEFAULT_OUTPUT_KEYS
+
+
+def get_animal_data(
+        animal: dict,
+        output_keys: tuple[str, ...] = DEFAULT_OUTPUT_KEYS
+) -> str:
+    """
+    Creates a string of animal data, including all the selected data.
+    :param animal: The animal data dictionary.
+    :param output_keys: Select data to be printed by the animal keys. Use ":" for nesting.
+    :return: The requested values a single string.
+    """
+    return "".join(
+        # Output in "key: value" manner
+        f"{key}: {value if type(value) is str else ", ".join(value)}\n"
+        for key, value in filter(
+            # Check if key has value
+            lambda zipped: zipped[1],
+
+            # Zip the correct key with its value
+            zip(
+                map(lambda key_string: key_string.split(":")[-1].capitalize(), output_keys),
+                map(lambda key_string: reduce(
+                    lambda dictionary, key: dictionary.get(key),
+                    key_string.split(":"),
+                    animal
+                ), output_keys)
+            )
+        )
+    )
